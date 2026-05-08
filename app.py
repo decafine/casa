e5b265562f94f10a905c5e67a32805a83c015d0c
decafine <wydha8y@gmail.com>
2026-05-08 06:17:03 +0000

    fix

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room
 import random
 import string
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "casa_secret"
 
 socketio = SocketIO(
     app,
-    cors_allowed_origins="*",
+    cors_allowed_origins="*"
 )
 
 waiting_player = None
-rooms = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-def generate_room_code(length=6):
-    return ''.join(
-        random.choice(string.ascii_uppercase)
-        for _ in range(length)
-    )
-
-
+# 랜덤 매치
 @socketio.on("join_random")
 def join_random():
 
@@ -41,14 +33,15 @@ def join_random():
 
     else:
 
-        room = generate_room_code()
+        room = ''.join(
+            random.choices(
+                string.ascii_uppercase + string.digits,
+                k=6
+            )
+        )
 
-        join_room(room, sid=sid)
         join_room(room, sid=waiting_player)
-
-        rooms[room] = {
-            "players":[waiting_player, sid]
-        }
+        join_room(room, sid=sid)
 
         socketio.emit(
             "matched",
@@ -56,7 +49,7 @@ def join_random():
                 "room":room,
                 "team":"red"
             },
-            room=waiting_player
+            to=waiting_player
         )
 
         socketio.emit(
@@ -65,36 +58,66 @@ def join_random():
                 "room":room,
                 "team":"blue"
             },
-            room=sid
+            to=sid
         )
 
         waiting_player = None
 
 
-@socketio.on("make_move")
-def make_move(data):
+# 친선전 생성
+@socketio.on("create_room")
+def create_room():
 
-    room = data.get("room")
+    room = ''.join(
+        random.choices(
+            string.ascii_uppercase + string.digits,
+            k=6
+        )
+    )
+
+    join_room(room)
 
     emit(
-        "opponent_move",
-        data,
-        room=room,
-        include_self=False
+        "room_created",
+        {
+            "room":room
+        }
     )
 
 
-@socketio.on("disconnect")
-def on_disconnect():
+# 친선전 참가
+@socketio.on("join_room_custom")
+def join_room_custom(data):
 
-    global waiting_player
+    room = data["room"]
 
-    sid = request.sid
+    join_room(room)
 
-    if waiting_player == sid:
-        waiting_player = None
+    emit(
+        "matched",
+        {
+            "room":room,
+            "team":"blue"
+        },
+        room=room
+    )
 
-    print(f"{sid} disconnected")
+
+# 이동 전달
+@socketio.on("make_move")
+def make_move(data):
+
+    room = data["room"]
+
+    emit(
+        "opponent_move",
+        {
+            "boardData":data["boardData"],
+            "currentTurn":data["currentTurn"]
+        },
+        room=room,
+        include_self=False
+    )
 
 
 if __name__ == "__main__":
